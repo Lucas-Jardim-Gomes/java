09332c2faf51e6d1e1842e4b124fccdac623ec27
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-01-02 10:35:39 -0300

    Create VariaveisTiposPrimitivos.java

---
+++ ViradoNoJiraya05/VariaveisTiposPrimitivos.java
@@ -0,0 +1,6 @@
+public class VariaveisTiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 22;
+        System.out.println("Lucas tem " + idade + " anos de idade");
+    }
+}
