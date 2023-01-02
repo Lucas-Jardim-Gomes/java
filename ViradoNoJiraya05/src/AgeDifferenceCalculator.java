e314a72750f754df6d8a87d0b7813763ba6b3aec
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-01-02 12:06:51 -0300

    atualização

--- ViradoNoJiraya05/src/AgeDifferenceCalculator.java
+++
@@ -1,8 +0,0 @@
-public class AgeDifferenceCalculator {
-    public static void main( String[] args){
-        int firstAge = 50;
-        int secondAge = 98;
-        int ageDifference = secondAge - firstAge;
-        System.out.println("the age difference is  " + ageDifference );
-}
-}
---
+++ ViradoNoJiraya05/src/CalculadorDiferencaIdade.java
@@ -0,0 +1,22 @@
+public class CalculadorDiferencaIdade {
+    public static void main(String[] args) {
+        int primeiraIdade = 22;
+        int segundaIdade = 30;
+        int diferencaIdade = Math.abs (primeiraIdade - segundaIdade) ;
+        System.out.println("A diferença de é " + diferencaIdade);
+    }
+}
+
+
+/*public class CalculadorDiferencaIdade {
+    public static void main( String[] args){
+        int firstAge = 50;
+        int secondAge = 98;
+        int ageDifference = secondAge - firstAge;
+        System.out.println("the age difference is  " + ageDifference );
+}
+}
+*/
+
+
+
