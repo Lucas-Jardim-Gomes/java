75cdc922e47cccaaaf2dca4d7ddfe9b191bd15cf
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-01-02 15:50:59 -0300

    atualização

--- ViradoNoJiraya05/src/CalculadorPorcentagemSalario.java
+++ ViradoNoJiraya05/src/CalculadorPorcentagemSalario.java
@@ -1,3 +1,4 @@
+/* 
 public class Aula {
     public static void main(String [] args) {
         float salary = 5000.00F;
@@ -7,5 +8,16 @@ public class Aula {
         System.out.println("15% eh: " + result);
         result = salary * 0.05F;
         System.out.println("5% eh: " + result);
+    }
+
+*/
+
+
+public class CalculadorPorcentagemSalario {
+    public static void main(String[] args) {
+        Float salario = 5000.00F;
+        float porcentagem = 0.3F;
+        float resultado = salario * porcentagem;
+        System.out.println("30% de 5000.00 é: " + resultado);
     }
 }
---
+++ ViradoNoJiraya05/src/SalarioPorcentagem.java
@@ -0,0 +1,28 @@
+/* 
+public class SalaryPercentage {
+public static void main(String[] args){
+    float salary = 5000.00F;
+    float result = salary * 0.3F;
+    System.out.println("30% de 5000.00 eh: " + result );
+    result = salary * 0.15F;
+    System.out.println("15% de 5000.00 eh: " + result);
+    result = salary * 0.05F;
+    System.out.println("5% de 5000.00 eh: " + result);
+}}
+*/
+
+
+
+
+
+public class SalarioPorcentagem {
+    public static void main(String[] args) {
+        var salario = 5000.00F;
+        float resultado = salario * 0.30F;
+        System.out.println("30% de 5000.00 é: " + resultado);
+        resultado = salario * 0.15F;
+        System.out.println("15% de 5000.00 é: " + resultado);
+        resultado = salario * 0.05F;
+        System.out.println("5% de 5000.00 é: " + resultado);
+    }
+}
