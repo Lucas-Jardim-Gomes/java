75069e1f5a2d99bb1066b37ffb9c9f81d3c2d1b0
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-01-06 10:29:16 -0300

    atualização

--- ViradoNoJiraya05/src/CadastroDeLogin.java
+++ ViradoNoJiraya05/src/CadastroDeLogin.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CadastroDeLogin {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Digite o seu nome");
+        System.out.println("Digite o seu nome: ");
         String nome = input.next();
     }
 }
