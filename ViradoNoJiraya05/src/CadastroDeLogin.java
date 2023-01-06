15b00cb5375d4c73863e1bfacc7bc66ec8d4e6fa
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-01-06 11:02:38 -0300

    Update CadastroDeLogin.java

--- ViradoNoJiraya05/src/CadastroDeLogin.java
+++ ViradoNoJiraya05/src/CadastroDeLogin.java
@@ -5,5 +5,10 @@ public class CadastroDeLogin {
         Scanner input = new Scanner(System.in);
         System.out.println("Digite o seu nome: ");
         String nome = input.next();
+        if (nome == "admin" || nome == "administrador"){
+            System.out.println("Usuário inválido");
+        } else {
+            System.out.println(nome + "cadastrado com sucesso");
+        }
     }
 }
